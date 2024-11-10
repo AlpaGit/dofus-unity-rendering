fb7df13dd5626fb9429932fa602529935e7c3474
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-11-11 00:58:29 +0100

    tmp

---
+++ components/Dropdown.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+  
+  <script setup lang="ts">  
+    class Option {
+        constructor(value, label) {
+            this.value = value;
+            this.label = label;
+        }
+    }
+    
+    const props = defineProps<{
+      options: Array<Option>,
+      placeholder: string,
+    }>()
+  
+
+  </script>
+  
+  
+  <style scoped lang="scss">
+  
+  </style>
